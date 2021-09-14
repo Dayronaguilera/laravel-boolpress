fb2b4cfd36c528a05b278b72600298464c7d779a
Dayronaguilera <aguilera.dayron@gmail.com>
2021-09-14 16:46:40 +0200

    errore

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home'); //colleghiamo HomeController
+
+Route::resources('posts', 'PostController'); //colleghiamo PostController
